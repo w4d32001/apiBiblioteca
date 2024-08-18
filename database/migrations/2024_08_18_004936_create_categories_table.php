f774f57c8848e1eee4bbd1cb73de7370f588fc33
w4d32001 <freedom01022001@gmail.com>
2024-08-17 20:29:01 -0500

    tabla de categorias

--- database/migrations/2024_08_18_004936_create_categories_table.php
+++ database/migrations/2024_08_18_004936_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
             $table->timestamps();
         });
     }
