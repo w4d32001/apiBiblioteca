48cf3652ea42b382144671d6520ad7e67c2d3e78
soledadoros <181226@unamba.edu.pe>
2024-08-18 18:16:38 -0500

    modelo de libro

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,49 @@
 
 namespace App\Models;
 
+use App\Traits\Audit;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
     use HasFactory;
+    use Audit;
+
+    protected $fillable = [
+        'name',
+        'isbm',
+        'price',
+        'publication_date',
+        'description',
+        'image',
+        'category_id',
+        'status'
+    ];
+    protected static function boot()
+    {
+        parent::boot();
+        static::bootAudit();
+    }
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function editor()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author():BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
 }
