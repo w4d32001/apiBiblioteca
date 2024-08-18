75c9135f33cd9a9b6a6119d63e9d656759c5d758
w4d32001 <freedom01022001@gmail.com>
2024-08-17 20:10:33 -0500

    primer commit

---
+++ app/Http/Controllers/helpers/BaseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\helpers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    //
+}
--- config/auth.php
+++ config/auth.php
@@ -14,10 +14,9 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Authentication Guards
@@ -40,7 +39,13 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver'=> 'jwt',
+            'provider'=> 'users',
+            'hash' => false,
+        ]
     ],
+
 
     /*
     |--------------------------------------------------------------------------
