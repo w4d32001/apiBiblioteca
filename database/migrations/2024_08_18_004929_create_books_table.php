cb8b61a3f7e9542498ca48d81183ac5eac9859e2
hectorcamven <212110@unamba.edu.pe>
2024-08-17 20:23:42 -0500

    tabla_libros

--- database/migrations/2024_08_18_004929_create_books_table.php
+++ database/migrations/2024_08_18_004929_create_books_table.php
@@ -13,10 +13,20 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->char('isbm')->unique();
+            $table->decimal('price', 10,2);
+            $table->date('publication_date');
+            $table->longText('description');
+            $table->longText('image');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
