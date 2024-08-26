af281863b67638e9c4fc2d57ddcd1e54cea92020
Kevin <kinn.n1998ags@gmail.com>
2024-08-25 21:42:46 -0500

    usercod

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -1,110 +1,56 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Models;
 
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\helpers\BaseController;
-use App\Http\Requests\Company\ImgRequest;
-use App\Http\Requests\User\StoreRequest;
-use App\Http\Requests\User\UpdateRequest;
-use App\Models\User;
-use Exception;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-use Illuminate\Support\Facades\Storage;
-
-class UserController extends BaseController
+class User extends Authenticatable implements JWTSubject
 {
-    public function index()
-    {
-        try {
-            $users = User::all();
-            return $this->sendResponse($users, 'Lista de usuarios');
-        } catch (Exception $e) {
-            return $this->sendError($e->getMessage())    ;
-        }
-    }
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * Store a newly created resource in storage.
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
      */
-    public function store(StoreRequest $request)
-    {
-        try {
-            $validated = $request->validated();
-            $validated['password'] = Hash::make($validated['password']);
-            $user = User::create($validated);
-            return $this->sendResponse($user, 'Usuario creado exitosamente','success', 201);
-        } catch (Exception $e) {
-            return $this->sendError($e->getMessage());
-        }
-    }
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'image'
+    ];
 
     /**
-     * Display the specified resource.
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
      */
-    public function show(User $user)
-    {
-        try {
-            return $this->sendResponse($user,'Usuario encontrado');
-        } catch (Exception $e) {
-            return $this->sendError($e->getMessage())    ;
-        }
-    }
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
-     * Update the specified resource in storage.
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
      */
-    public function update(UpdateRequest $request, User $user)
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+    public function getJWTIdentifier()
     {
-        try {
-            $validated = $request->validated();
-            if (!empty($validated['password'])) {
-                $validated['password'] = Hash::make($validated['password']);
-            }else{
-                $validated['password'] = $user->password;
-            }
-            $user->update($validated);
-            return $this->sendResponse($user, 'Usuario actualizado correctamente');
-        } catch (Exception $e) {
-            return $this->sendError($e->getMessage())    ;
-        }
+        return $this->getKey();
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
+    public function getJWTCustomClaims()
     {
-        try {
-            $user->delete();
-            return $this->sendResponse([], 'Usuario eliminado exitosamente');
-        } catch (Exception $e) {
-            return $this->sendError($e->getMessage());
-        }
+        return [];
     }
-
-    public function updateImage(ImgRequest $request, User $user)
-    {
-
-        $validated = $request->validated();
-
-        try {
-            if ($request->hasFile('image')) {
-
-                $path = $request->file('image')->store('public/images');
-                $imageUrl = Storage::url($path);
-                $baseUrl = url('/');
-                $validated['image'] = $baseUrl . $imageUrl;
-
-                $user->update(['image' => $validated['image']]);
-                return $this->sendResponse($user, 'Imagen actualizada exitosamente');
-            } else {
-                return $this->sendError('No se encontró la imagen en la solicitud.');
-            }
-        } catch (Exception $e) {
-            return $this->sendError($e->getMessage());
-        }
-    }
+    
 }
