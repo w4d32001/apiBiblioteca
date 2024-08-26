20115d27d476fbddf6601351f7cc23091a583657
Kevin <kinn.n1998ags@gmail.com>
2024-08-25 21:33:00 -0500

    categoria ps

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -1,95 +1,40 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Models;
 
-use App\Http\Controllers\helpers\BaseController;
-use App\Http\Requests\Category\StoreRequest;
-use App\Http\Requests\Category\UpdateRequest;
-use App\Http\Resources\CategoryResource;
-use App\Models\Category;
-use Exception;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
+use App\Traits\Audit;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class CategoryController extends BaseController
+class Category extends Model
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    use HasFactory;
+    use Audit;
+
+    protected $fillable = [
+        'name',
+        'created_by',
+        'updated_by'
+    ];
+
+    protected static function boot()
     {
-        try{
-            $categories = Category::with('editor', 'creator')->get();
-            return $this->sendResponse(CategoryResource::collection($categories), 'Lista de categorías');
-        }catch(Exception $e){
-            return $this->sendError($e->getMessage());
-        }
+        parent::boot();
+        static::bootAudit();
+    }
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreRequest $request)
+    public function editor()
     {
-        try{
-            $validated = $request->validated();
-            $validated['created_by'] = Auth::id();
-            $validated['updated_by'] = $validated['created_by'];
-            $category = Category::create($validated);
-            
-            return $this->sendResponse($category, 'Categoria creada exitosamente', 'success', 201);
-
-        }catch(Exception $e){
-            return $this->sendError($e->getMessage());
-        }
+        return $this->belongsTo(User::class, 'updated_by');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Category $category)
+    public  function books(): HasMany
     {
-        try{
-            return $this->sendResponse($category, 'Categoria encontrada exitosamente');
-        }catch(Exception $e){
-            return $this->sendError($e->getMessage());
-        }
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateRequest $request, Category $category)
-    {
-        try {
-            $validated = $request->validated();
-            $validated['updated_by'] = Auth::id();
-            $category->update($validated);
-            return $this->sendResponse($category, 'Categoria actualizada exitosamente');
-        } catch (Exception $e) {
-            return $this->sendError($e->getMessage());
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Category $category)
-    {
-        try
-        {
-            if($category->books()->exists())
-            {
-                return $this->sendError('Este dato tiene otro dato asociado, no se puede eliminar');
-            }
-            else{
-                $category->delete();
-                return $this->sendResponse([], 'Categoria eliminada exitosamente');
-            }
-        }
-        catch(Exception $e)
-        {
-            return $this->sendError($e->getMessage());
-        }
+        return $this->hasMany(Book::class);
     }
 }
